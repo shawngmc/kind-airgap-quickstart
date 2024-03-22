46ed374b013dba213a32e5ba45e7a3436fc48b5e
Shawn McNaughton <shawn@k3s-playground.local.hitoma.com>
2024-03-22 02:54:10 -0400

    kube-ps1 fix

--- install/030-install-kubeps1.sh
+++ install/030-install-kubeps1.sh
@@ -6,5 +6,5 @@ echo "Staging Kube-PS1 executable..."
 sudo install ${GAP_DIR}/kube-ps1 /usr/local/bin/
 
 echo "Adding Kube-PS1 to .bashrc..."
-echo "source /usr/local/bin/kube-ps1.sh" >> ~/.bashrc
+echo "source /usr/local/bin/kube-ps1" >> ~/.bashrc
 echo "PS1='[\u@\h \W "'$(kube_ps1)'"]\$ '" >> ~/.bashrc
